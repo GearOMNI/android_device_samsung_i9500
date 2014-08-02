e67f89be034f6230912c39d94c6a6f7ab01cbec8
Alberto96 <albertoguanti@gmail.com>
2014-08-02 13:24:49 +0200

    i9500: Final bringup - making it build in Omni build
    part 4

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,4 +1,5 @@
-# Copyright (C) 2013 The CyanogenMod Project
+#
+# Copyright (C) 2011 The Android Open-Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,16 +12,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-# Inherit from i9500 device
-$(call inherit-product, device/samsung/i9500/device.mk)
-
-# Discard inherited values and use our own instead.
-PRODUCT_NAME := full_i9500
-PRODUCT_DEVICE := i9500
-PRODUCT_BRAND := samsung
-PRODUCT_MANUFACTURER := Samsung
-PRODUCT_MODEL := GT-I9500
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_i9500.mk \
+    $(LOCAL_DIR)/omni_i9500.mk
--- omni_i9500.mk
+++ omni_i9500.mk
@@ -1,10 +1,11 @@
-# Copyright (C) 2013 The CyanogenMod Project
+# Copyright (C) 2013 OmniROM Project
+# Copyright (C) 2012 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,15 +13,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit from those products. Most specific first.
+# Inherit Omni GSM telephony parts
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from our omni product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from i9500 device
 $(call inherit-product, device/samsung/i9500/device.mk)
 
-# Discard inherited values and use our own instead.
-PRODUCT_NAME := full_i9500
+PRODUCT_NAME := omni_i9500
 PRODUCT_DEVICE := i9500
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := Samsung
 PRODUCT_MODEL := GT-I9500
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_MODEL=GT-I9500 \
+    PRODUCT_NAME=ja3gxx \
+    PRODUCT_DEVICE=ja3g \
+    TARGET_DEVICE=ja3g \
+    BUILD_FINGERPRINT="samsung/ja3gxx/ja3g:4.4.2/KOT49H/I9500XXUGNG3:user/release-keys" \
+    PRIVATE_BUILD_DESC="ja3gxx-user 4.4.2 KOT49H I9500XXUGNG3 release-keys"
